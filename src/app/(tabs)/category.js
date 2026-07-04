006f5f26e1b9f6fff73a434e7c00717f16c05e1f
Fasika <fasika394@gmail.com>
2026-07-04 03:40:20 +0300

    Add searchInput to category layout"

--- src/app/(tabs)/category.js
+++ src/app/(tabs)/category.js
@@ -3,8 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/header';
 import useTheme from '../../store/useTheme';
-
-<<<<<<< HEAD
+import SearchInput from '../../components/searchInput';
 
 const category = () => {
     const { colors, fontSize, spacing } = useTheme();
@@ -13,25 +12,12 @@ const category = () => {
        
         <SafeAreaView style={styles.container}>
             <Header header={'category'}/>
-=======
-const Category = () => {
-    const { colors, fontSize, spacing } = useTheme();
-    const styles = createStyles(colors, fontSize, spacing);
-    return (
-        <SafeAreaView style={styles.container}>
-            <Header header={'category'} />
->>>>>>> searchInput
         </SafeAreaView>
     );
 }
 
-<<<<<<< HEAD
 const createStyles = (colors,fontSize,spacing) => StyleSheet.create({
     container:{
-=======
-const createStyles = (colors, fontSize, spacing) => StyleSheet.create({
-    container: {
->>>>>>> searchInput
         backgroundColor: colors.background,
         flex: 1,
     }
